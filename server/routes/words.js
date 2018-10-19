046787632cbc15c16b4a039672f4c11df4e06c7d
Jan Václavík <jvaclavik@gmail.com>
2018-10-19 17:54:12 +0200

    Server: Create basic structure

---
+++ server/routes/words.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const getCombinations = (input) => {
+  // @TODO
+  return input;
+}
+
+router.get('/', (req, res, next) => 
+  res
+    .status(200)
+    .json(
+      getCombinations(req.query.input)
+    )
+);
+
+module.exports = router;
+
